f44caafe9a49eec6bf65f5cbc54ea08cd55d38f7
abdullahfakhri <abdullahfakhri17@gmail.com>
2022-03-11 21:17:25 +0700

    push baru

--- soal6.jsx
+++ soal6.jsx
@@ -1,16 +1,17 @@
 const dataAngka = [9,4,7,7,4,3,2,2,8]
 
-function getAngkaTerbesarKedua (personName){
-    let angka1 = array.isArray(personName)
+function getAngkaTerbesarKedua(personName) { 
+    let angka1 = Array.isArray(personName) 
     if (angka1 == true){
-        return "true"
-    }else{
-        return "false"
+
+    } else {
+        return "Error: parameter bukan array"
     }
     let angka2 = personName.sort ((a, b) => {
         return b - a
     });
     return angka2 [1]
+    
 }
 
 console.log(getAngkaTerbesarKedua(dataAngka));
--- soal8.jsx
+++ soal8.jsx
@@ -1,75 +1,72 @@
-const dataPenjualanNovel = [
-    {
-        idProduct: 'BOOK002421',
-        namaProduk: 'Pulang-Pergi',
-        penulis: 'Tere Liye',
-        hargaBeli: 60000,
-        hargaJual: 86800,
-        totalTerjual: 150,
-        sisaStock: 17,
-    },
-    {
-        idProduct: 'BOOK002351',
-        namaProduk: 'Selamat Tinggal',
-        penulis: 'Tere Liye',
-        hargaBeli: 75000,
-        hargaJual: 103000,
-        totalTerjual: 171,
-        sisaStock: 20,
-    },
-    {
-        idProduct: 'BOOK002941',
-        namaProduk: 'Garis Waktu',
-        penulis: 'Fiersa Besari',
-        hargaBeli: 67000,
-        hargaJual: 99000,
-        totalTerjual: 213,
-        sisaStock: 5,
-    },
-    {
-        idProduct: 'BOOK002941',
-        namaProduk: 'Laskar Pelangi',
-        penulis: 'Andrea Hitari',
-        hargaBeli: 55000,
-        hargaJual: 68000,
-        totalTerjual: 20,
-        sisaStock: 56,
-    },
-]
+const dataPenjualanNovel = [ 
+    { 
+        idProduct:'BOOK002421', 
+        namaProduct: 'Pulang-Pergi', 
+        penulis: 'Tere Liye', 
+        hargaBeli: 60000, 
+        hargaJual:86000, 
+        totalTerjual: 150, 
+        sisaStok: 17, 
+    }, 
+    { 
+        idProduct:'BOOK002351', 
+        namaProduct: 'Selamat Tinggal', 
+        penulis: 'Tere Liye', 
+        hargaBeli: 75000, 
+        hargaJual:103000, 
+        totalTerjual: 171, 
+        sisaStok: 20, 
+    }, 
+    { 
+        idProduct:'BOOK002941', 
+        namaProduct: 'Garis Waktu', 
+        penulis: 'Fiersa Besari', 
+        hargaBeli: 67000, 
+        hargaJual:99000, 
+        totalTerjual: 213, 
+        sisaStok: 5, 
+    }, 
+    { 
+        idProduct:'BOOK002941', 
+        namaProduct: 'Laskar Pelangi', 
+        penulis: 'Andrea Hirata', 
+        hargaBeli: 55000, 
+        hargaJual:68000, 
+        totalTerjual: 20, 
+        sisaStok: 56, 
+    }, 
+] 
+ 
+function getInfoPenjualan(dataPenjualanNovel) { 
+    const total ={ 
+        totalKeuntungan:0, 
+        totalModal:0, 
+        presentaseKeuntungan:0, 
+        produkBukuTerlaris:"", 
+        penulisTerlaris:"", 
+    } 
 
-function getInfoPenjualan(total) {
-    let totalKeuntungan = 0
-    let totalModal = 0
-    let persentaseKeuntungan = 0
-    let produkBukuTerlaris = 0
-    let penulisTerlaris = 0
-    total.map((value, index) => {
-        totalKeuntungan += (value.hargaJual * value.totalTerjual) - ((value.totalTerjual + value.sisaStock) * value.hargaBeli)
-        totalModal += (value.totalTerjual + value.sisaStock) * value.hargaBeli
-        persentaseKeuntungan += ((value.totalTerjual + value.sisaStock) * value.hargaJual) - ((value.totalTerjual + value.sisaStock) * value.hargaBeli) / ((value.totalTerjual + value.sisaStock) * value.hargaBeli) * 100
-    })
-    produkBukuTerlaris = total.reduce((previousValue, currentValue) => {
-        if (previousValue.totalTerjual > currentValue.totalTerjual){
-            return previousValue.namaProduk
-        }else{
-            return currentValue.namaProduk
-        }
-    });
-    penulisTerlaris = total.reduce((previousValue, currentValue) => {
+    let totalStok=0; 
+    let totalTerjualSekarang=0; 
+    let bukuTerlarisSekarang=0; 
+ 
+    dataPenjualanNovel.map((item)=>{ 
+        totalStok =item.totalTerjual + item.sisaStok 
+        total.totalKeuntungan += item.hargaJual - item.hargaBeli; 
+        total.totalModal += totalStok * item.hargaBeli; 
+        total.presentaseKeuntungan = total.totalModal/item.hargaBeli 
 
-        if (previousValue.totalTerjual > currentValue.totalTerjual){
-            return previousValue.penulis
-        }else{
-            return currentValue.penulis
-        }
-    });
-
-    return (totalKeuntungan)
-    return (totalModal)
-    return (persentaseKeuntungan)
-    return (produkBukuTerlaris)
-    return (penulisTerlaris)
-    
-
-}
+        if(item.totalTerjual>totalTerjualSekarang){ 
+            total.produkBukuTerlaris= item.namaProduct 
+            totalTerjualSekarang = item.totalTerjual 
+        } 
+ 
+        if(item.totalTerjual>bukuTerlarisSekarang){ 
+            total.penulisTerlaris= item.penulis 
+            bukuTerlarisSekarang = item.totalTerjual 
+        } 
+    }); 
+ 
+    return total 
+} 
 console.log(getInfoPenjualan(dataPenjualanNovel));
